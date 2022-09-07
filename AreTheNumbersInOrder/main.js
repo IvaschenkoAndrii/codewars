29499a56478d61502474cbcbce68ef2b01e07e88
IvaschenkoAndrii <ivaschenko84@gmail.com>
2022-09-07 20:08:37 +0300

    codeWars

--- AreTheNumbersInOrder/main.js
+++ AreTheNumbersInOrder/main.js
@@ -1,8 +1,7 @@
 function inAscOrder(arr) {
-    let res = arr.filter((value, index, array) => array[index + 1] < array[index]);
-    return !res.length;
+    return !(arr.filter((value, index, array) => array[index + 1] < array[index])).length;
 }
 
 
-arraay = [1, 6, 10, 18, 2, 4, 20];
+arraay = [1, 6, 10, 18];
 console.log(inAscOrder(arraay));
