416a721371c183c0d1046aa60d58d06ba3c3c417
IvaschenkoAndrii <ivaschenko84@gmail.com>
2022-09-10 00:00:03 +0300

    codeWars

---
+++ BackAndForthThenReverse!/main.js
@@ -0,0 +1,25 @@
+// function arrange(s) {
+//     let res = [];
+//     let l = s.length/2;
+//     for (let i = 0; i < l; i++) {
+//         s.length !== 1 ? res.push(s[0], s[s.length - 1]) : res.push(s[0]);
+//         s = s.slice(1, s.length - 1).reverse();
+//     }
+//     return res;
+// }
+
+
+function arrange(s) {
+    let res = [];
+    let counter = 0;
+    let l = s.length/2;
+    for (let i = 0; i < l; i++) {
+        counter++;
+        s.length !== 1 ? (counter%2?res.push(s[0], s[s.length - 1]) : res.push(s[s.length-1],s[0])) : res.push(s[0]);
+        s = s.slice(1, s.length - 1);
+    }
+    return res;
+}
+
+let a = [-2,5,1,-10,2,-9,-1,-2,-2,3,-10,-4,4,6];
+console.log(arrange(a));
